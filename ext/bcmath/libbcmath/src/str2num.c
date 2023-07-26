685f3ee8757c9167adf710853a110f1bb4552b74
Jorg Adam Sowa <jorg.sowa@gmail.com>
2023-07-26 14:48:50 +0200

    Fix GH-11761: Bcmath numbers with trailing zeros  (#11798)

--- ext/bcmath/libbcmath/src/str2num.c
+++ ext/bcmath/libbcmath/src/str2num.c
@@ -75,8 +75,18 @@ bool bc_str2num (bc_num *num, char *str, size_t scale)
 	}
 	/* digits after the decimal point */
 	while (*ptr >= '0' && *ptr <= '9') {
+		if (*ptr == '0') {
+			trailing_zeros++;
+		} else {
+			trailing_zeros = 0;
+		}
 		ptr++;
 		strscale++;
+	}
+
+	if (trailing_zeros > 0) {
+		/* Trailining zeros should not take part in the computation of the overall scale, as it is pointless. */
+		strscale = strscale - trailing_zeros;
 	}
 	if ((*ptr != '\0') || (digits+strscale == 0)) {
 		*num = bc_copy_num (BCG(_zero_));
